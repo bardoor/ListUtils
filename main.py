80f12a28f605aa9ee5296f133e03efce1deb3d17
chekau <m_cherchiev_08@mail.ru>
2024-01-17 18:02:09 +0300

    Доработка кода random_len_lst
    проверка на исключении введённых пользователем числа

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from random import randint
 
 
-def random_lst(n: int, from_num: int, to_num: int) -> list:
+def random_lst(num: int, from_num: int, to_num: int) -> list:
     lst = []
-    
-    for i in range(n):
-        lst.append(randint(from_num,to_num))
-    return lst
+    if type(num) != int:
+        raise ValueError("Вы ввели не целое число")
+    elif type(from_num) != int:
+        raise ValueError("Вы ввели не целое число")
+    elif type(to_num) != int:
+        raise ValueError("Вы ввели не целое число")
+    else:
+        for i in range(num):
+            lst.append(randint(from_num,to_num))
+        return lst
 
