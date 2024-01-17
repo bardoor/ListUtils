a5ad651bc4f9625711071527efc2e867f4dd92c6
zente1ku <egor.egor.3.timofeev@mail.ru>
2024-01-17 17:57:42 +0300

    исправленный Find element
    Ищет элемент внутри списка

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-lst = [1132,22342,3453,4235]
-target = int(input())
 def find_element(lst,target):
     index = -1
     for i in range(len(lst)):
         if lst[i] == target:
             index = i  
     return index
-print(find_element(lst,target))
+
